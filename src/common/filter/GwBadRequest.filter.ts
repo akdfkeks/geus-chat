29fcb10d328f4578401a4c14080745f7d0f6cf0f
akdfkeks <akdfkeks@gmail.com>
2024-01-09 14:45:23 +0900

    fix: 예외를 위임하지 않고 직접 처리하도록 변경
    우선순위가 높은 filter에서 예외를 던지면 낮은 순위의 filter에 위임되지 않고 애플리케이션이 중단됩니다.
    따라서 TypeGuard Exception (BadRequest) 를 처리하는 필터에서 직접 예외를 처리하도록 변경하였습니다.

--- src/common/filter/GwBadRequest.filter.ts
+++ src/common/filter/GwBadRequest.filter.ts
@@ -1,13 +1,21 @@
 import { ArgumentsHost, Catch } from '@nestjs/common';
 import { BaseWsExceptionFilter } from '@nestjs/websockets';
 import { log } from 'console';
+import { Socket } from 'socket.io';
 import { GatewayException, INVALID_FORMAT } from 'src/structure/dto/Exception';
+import { SendOP } from 'src/structure/dto/Message';
 import { TypeGuardError } from 'typia';
 
 @Catch(TypeGuardError)
 export class GwBadRequestFilter extends BaseWsExceptionFilter {
   catch(exception: TypeGuardError, host: ArgumentsHost) {
-    log(exception);
-    throw new GatewayException(INVALID_FORMAT);
+    const clientSocket = host.switchToWs().getClient<Socket>();
+    clientSocket.send({
+      op: SendOP.ERROR,
+      d: {
+        code: INVALID_FORMAT.code,
+        message: INVALID_FORMAT.message,
+      },
+    });
   }
 }
