a25e6ca7f89e2b3e675ae7c1f1625164d551dc64
akdfkeks <akdfkeks@gmail.com>
2024-02-02 15:05:22 +0900

    feat: epoch 및 date parsing method 추가
    id를 생성할 때 custom epoch를 사용하도록 변경하였습니다.
    snowflake id로부터 날짜를 추출하는 메서드를 추가하였습니다.

--- src/common/util/snowflake.ts
+++ src/common/util/snowflake.ts
@@ -1,9 +1,15 @@
 import * as Flake from 'flake-idgen';
 
-const flake = new Flake();
+export const GEUS_EPOCH = 1704067200000; //01/01/2024, 00:00:00 (UTC +00:00)
+
+const flake = new Flake({ worker: process.pid, epoch: GEUS_EPOCH });
 
 export namespace SnowFlake {
   export const generate = () => {
     return BigInt('0x' + flake.next().toString('hex'));
+  };
+  export const parseDate = (snowflake: number | bigint) => {
+    const ms = (BigInt(snowflake) >> 22n) as unknown as number;
+    return new Date(ms + GEUS_EPOCH);
   };
 }
