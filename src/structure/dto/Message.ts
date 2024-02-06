b370454e9fb118f049f87cea03c01ebfbe33c226
akdfkeks <akdfkeks@gmail.com>
2024-02-06 16:27:53 +0900

    refact: 응답 메세지 형식 변경

--- src/structure/dto/Message.ts
+++ src/structure/dto/Message.ts
@@ -1,34 +1,5 @@
 import { tags } from 'typia';
 
-export namespace RecvPayload {
-  export interface Text {
-    channelId: string & tags.Pattern<'^[0-9A-HJKMNP-TV-Z]{26}$'>;
-    message: string & tags.MinLength<1> & tags.MaxLength<2000>;
-  }
-  export interface Identify {
-    accessToken: string;
-  }
-}
-
-export namespace SendPayload {
-  export interface Text {
-    channelId: string;
-    message: string;
-    sender: {
-      id: number;
-    };
-  }
-  export interface UpdateChannel {
-    channelId: string;
-    [key: string]: any;
-  }
-}
-
-export interface Message<PayloadType = any> {
-  op: number & tags.Minimum<0> & tags.Maximum<100>;
-  d: PayloadType;
-}
-
 export enum SendOP {
   DISPATCH_MESSAGE = 0,
   DISPATCH_IMAGES = 1,
@@ -42,4 +13,40 @@ export enum RecvOP {
   SEND_MESSAGE = 0,
   IDENTIFY = 10,
   RECONNECT = 11,
+}
+
+export enum MessageType {
+  TEXT = 0,
+  FILE = 1,
+}
+
+export namespace RecvPayload {
+  export interface Text {
+    cid: string & tags.Pattern<'^[0-9A-HJKMNP-TV-Z]{26}$'>;
+    data: string & tags.MinLength<1> & tags.MaxLength<2000>;
+  }
+  export interface Identify {
+    token: string;
+  }
+}
+
+export namespace SendPayload {
+  export interface Text {
+    mid: bigint; // Message ID
+    cid: string; // Channel ID
+    mtype: MessageType; // Message Type {0: Text, 1: Image, 2: File, ...}
+    data: string;
+    uid: number; // User ID
+    uname: string; // User Name
+  }
+
+  export interface UpdateChannel {
+    cid: string;
+    [key: string]: any;
+  }
+}
+
+export interface Message<PayloadType = any> {
+  op: number & tags.Minimum<0> & tags.Maximum<100>;
+  d: PayloadType;
 }
