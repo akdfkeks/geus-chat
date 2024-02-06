aeb3ad49f04e2ff6fb43505ac78492606cdba3b8
akdfkeks <akdfkeks@gmail.com>
2024-02-06 16:42:10 +0900

    feat: snowflake id generator 추가

--- src/common/util/snowflake.ts
+++ src/common/util/snowflake.ts
@@ -5,8 +5,13 @@ export const GEUS_EPOCH = 1704067200000; //01/01/2024, 00:00:00 (UTC +00:00)
 const flake = new Flake({ worker: process.pid, epoch: GEUS_EPOCH });
 
 export namespace SnowFlake {
-  export const generate = () => {
-    return BigInt('0x' + flake.next().toString('hex'));
+  export const generate = (): bigint => {
+    try {
+      return BigInt('0x' + flake.next().toString('hex'));
+    } catch (e) {
+      // 계속 실패하면 콜스택 터지나
+      return SnowFlake.generate();
+    }
   };
   export const parseDate = (snowflake: number | bigint) => {
     const ms = (BigInt(snowflake) >> 22n) as unknown as number;
