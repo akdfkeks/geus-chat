3fca6225d5094180bc129b2bb1676672d99d0a9a
akdfkeks <akdfkeks@gmail.com>
2024-02-14 02:10:18 +0900

    feat: 사용자 검색 메서드

--- src/repository/user.repository.ts
+++ src/repository/user.repository.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/service/prisma.service';
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  public async findUserById(id: number) {
+    return this.prisma.gh_User.findUniqueOrThrow({
+      where: { user_id: id },
+    });
+  }
 
   public async findUserByEmail(email: string) {
     return this.prisma.gh_User.findUnique({ where: { email } });
