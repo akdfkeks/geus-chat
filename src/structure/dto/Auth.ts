01b2d58da93fce2c3c3a466745f2e27004e0bbaf
akdfkeks <akdfkeks@gmail.com>
2024-02-21 08:58:22 +0900

    fix: (커밋 누락) 로그인 요청 검증을 위한 인터페이스 추가

--- src/structure/dto/Auth.ts
+++ src/structure/dto/Auth.ts
@@ -1,3 +1,12 @@
+import typia from 'typia';
+
+import * as tags from 'typia/lib/tags';
+
 export interface JWTPayload {
   uid: number;
+}
+
+export interface IUserLoginDto {
+  email: string & tags.MinLength<1> & tags.MaxLength<50>; // & tags.Format<'email'>;
+  password: string & tags.MinLength<1> & tags.MaxLength<50> & tags.Format<'password'>;
 }
