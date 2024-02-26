3ad721df17aca16a3214928cb073c138a7ddee0e
akdfkeks <akdfkeks@gmail.com>
2024-02-26 16:24:06 +0900

    refact: redis 연결정보를 환경변수에서 불러오도록 변경

--- src/module/cache.module.ts
+++ src/module/cache.module.ts
@@ -1,17 +1,23 @@
 import { RedisModule } from '@liaoliaots/nestjs-redis';
-export const CacheModule = RedisModule.forRoot({
-  config: [
-    {
-      host: 'localhost',
-      port: 6379,
-      namespace: 'message_channel_list',
-      lazyConnect: true,
-    },
-    {
-      host: 'localhost',
-      port: 6379,
-      namespace: 'socket_user_map',
-      lazyConnect: true,
-    },
-  ],
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const CacheModule = RedisModule.forRootAsync({
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: async (config: ConfigService) => ({
+    config: [
+      {
+        host: config.get('REDIS_HOST'),
+        port: config.get('REDIS_PORT'),
+        namespace: 'message_channel_list',
+        lazyConnect: true,
+      },
+      {
+        host: config.get('REDIS_HOST'),
+        port: config.get('REDIS_PORT'),
+        namespace: 'socket_user_map',
+        lazyConnect: true,
+      },
+    ],
+  }),
 });
