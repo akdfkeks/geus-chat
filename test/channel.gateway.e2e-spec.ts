88b6b8d51208ee4107a9ed99e2f0c501cde4b57e
akdfkeks <akdfkeks@gmail.com>
2024-03-08 20:50:09 +0900

    test: 테스트용 클라이언트 생성 메서드 추가

--- test/channel.gateway.e2e-spec.ts
+++ test/channel.gateway.e2e-spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/module/app.module';
 import { Socket, io } from 'socket.io-client';
-import { RecvOP, SendOP } from 'src/structure/dto/Message';
 import { RedisIoAdapter } from 'src/common/adapter/redis.adapter';
 import { ConfigService } from '@nestjs/config';
-import { RecvPayload, Message } from 'src/structure/dto/Message';
+import { EventData } from 'src/structure/dto/Event';
 import * as error from 'src/structure/dto/Exception';
-import * as request from 'supertest';
-import { BigIntegerUtil } from 'src/common/util/bInteger.util';
 import { SnowFlake } from 'src/common/util/snowflake';
+import { RecvOP, SendOP } from 'src/common/constant/message';
+import { Message } from 'src/structure/message';
+import { TestClient, getChannels, getToken } from 'test/common/Client';
 
 describe('[Socket] ChannelGateway (e2e)', () => {
   let app: INestApplication;
@@ -17,11 +17,14 @@ describe('[Socket] ChannelGateway (e2e)', () => {
 
   let NODE_HOST: string;
   let NODE_PORT: string;
+  let baseURL: string;
 
   let sender: Socket;
   let receiver: Socket;
   let senderAccess: string;
   let receiverAccess: string;
+  let senderChannels: Array<string>;
+  let receiverChannels: Array<string>;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -35,33 +38,24 @@ describe('[Socket] ChannelGateway (e2e)', () => {
 
     NODE_HOST = configService.get('NODE_HOST') || 'localhost';
     NODE_PORT = configService.get('NODE_PORT') || '3000';
+    baseURL = `${NODE_HOST}:${NODE_PORT}`;
 
     await (await app.init()).listen(NODE_PORT);
   });
 
   beforeAll(async () => {
-    sender = io('http://' + NODE_HOST + ':' + NODE_PORT + '/v2/channel', {
-      autoConnect: true,
-      forceNew: true,
-    });
-    receiver = io('http://' + NODE_HOST + ':' + NODE_PORT + '/v2/channel', {
-      autoConnect: true,
-      forceNew: true,
-    });
-    senderAccess = await request(app.getHttpServer())
-      .post('/v1/auth/login')
-      .send({
-        email: 'alice@example.com',
-        password: 'test',
-      })
-      .then(({ body }) => body.accessToken);
-    receiverAccess = await request(app.getHttpServer())
-      .post('/v1/auth/login')
-      .send({
-        email: 'bob@example.com',
-        password: 'test',
-      })
-      .then(({ body }) => body.accessToken);
+    [senderAccess, receiverAccess] = await Promise.all([
+      getToken(baseURL, { email: 'alice@example.com', password: 'test' }),
+      getToken(baseURL, { email: 'bob@example.com', password: 'test' }),
+    ]);
+
+    [senderChannels, receiverChannels] = await Promise.all([
+      getChannels(baseURL, senderAccess),
+      getChannels(baseURL, receiverAccess),
+    ]);
+
+    sender = TestClient(`${baseURL}/v2/channel`, { auth: senderAccess });
+    receiver = TestClient(`${baseURL}/v2/channel`, { auth: receiverAccess });
   });
 
   afterAll(async () => {
@@ -71,100 +65,45 @@ describe('[Socket] ChannelGateway (e2e)', () => {
   });
 
   describe('클라이언트 인증', () => {
-    let client: Socket;
-    let accessToken: string;
-
-    beforeAll(async () => {
-      client = io('http://' + NODE_HOST + ':' + NODE_PORT + '/v2/channel', {
-        autoConnect: true,
-        forceNew: true,
-      });
-      accessToken = await request(app.getHttpServer())
-        .post('/v1/auth/login')
-        .send({
-          email: 'alice@example.com',
-          password: 'test',
-        })
-        .then(({ body }) => body.accessToken);
-    });
-
     it('잘못된 인증 형식을 전송하면 에러를 반환합니다.', async () => {
+      const client = TestClient(`${baseURL}/v2/channel`, { autoConnect: false });
       return new Promise<void>((res, rej) => {
-        client.send({
-          op: RecvOP.IDENTIFY,
-          d: { access: accessToken, unknownField: 'lololo' },
-        } satisfies Message);
-
-        client.on('message', (data) => {
+        client.connect();
+        client.on('message', (data: any) => {
           if (data.op !== SendOP.ERROR) return;
           expect(data.d.code).toBe(error.INVALID_FORMAT.code);
+          client.disconnect();
           res();
         });
       });
     });
 
-    it('인증에 성공하면 참여중인 채널 정보를 수신합니다.', async () => {
+    it('인증에 성공하면 사용자 ID를 수신합니다.', async () => {
+      const client = TestClient(`${baseURL}/v2/channel`, { auth: senderAccess, autoConnect: false });
       return new Promise<void>((res, rej) => {
-        client.send({
-          op: RecvOP.IDENTIFY,
-          d: { token: accessToken },
-        } satisfies Message<RecvPayload.Identify>);
-
-        client.on('message', (data) => {
-          if (data.op === SendOP.HELLO && data.d.channels) res();
+        client.connect();
+        client.on('message', (data: any) => {
+          if (data.op !== SendOP.HELLO) return;
+          expect(data.d.id).toBeDefined();
+          client.disconnect();
+          res();
         });
       });
     });
-
-    afterAll(() => client.disconnect());
   });
 
   describe('메세지 전송/수신', () => {
-    let senderChannels: Array<string>;
-    let receiverChannels: Array<string>;
-    beforeAll(async () => {
-      return await Promise.all([
-        new Promise<void>((res, rej) => {
-          sender.send({
-            op: RecvOP.IDENTIFY,
-            d: { token: senderAccess },
-          } satisfies Message<RecvPayload.Identify>);
-
-          sender.on('message', (data) => {
-            if (data.op === SendOP.HELLO) {
-              senderChannels = data.d.channels;
-              res();
-            }
-          });
-        }),
-        new Promise<void>((res, rej) => {
-          receiver.send({
-            op: RecvOP.IDENTIFY,
-            d: { token: receiverAccess },
-          } satisfies Message<RecvPayload.Identify>);
-
-          receiver.on('message', (data) => {
-            if (data.op === SendOP.HELLO) {
-              receiverChannels = data.d.channels;
-              res();
-            }
-          });
-        }),
-      ]);
-    });
-
     it('참여중이지 않은 채널에 전송을 시도하면 오류를 반환합니다.', async () => {
       return new Promise<void>((res, rej) => {
-        sender.send({
+        sender.emit('event', {
           op: RecvOP.SEND_MESSAGE,
           d: {
             cid: SnowFlake.generate().toString(),
             data: 'hello',
           },
-        } satisfies Message<RecvPayload.Text>);
+        } satisfies EventData<Message.RecvDto>);
 
-        sender.on('message', (data) => {
-          console.log(data);
+        sender.on('message', (data: any) => {
           if (data.op !== SendOP.ERROR) return;
           if (data.d.code === error.NO_PERMISSION.code) {
             res();
@@ -174,7 +113,7 @@ describe('[Socket] ChannelGateway (e2e)', () => {
     });
 
     it('전송 성공 시 전송한 메세지를 수신합니다.', async () => {
-      const testMessage: Message<RecvPayload.Text> = {
+      const testMessage: EventData<Message.RecvDto> = {
         op: RecvOP.SEND_MESSAGE,
         d: {
           cid: senderChannels[0],
@@ -183,38 +122,14 @@ describe('[Socket] ChannelGateway (e2e)', () => {
       };
 
       return new Promise<void>((res, rej) => {
-        sender.send(testMessage);
-        sender.on('message', (data) => {
-          if (data.op === SendOP.DISPATCH_MESSAGE) {
-            expect(data.d.cid).toBe(testMessage.d.cid);
-            res();
-          } else rej();
+        sender.emit('event', testMessage);
+        sender.on('message', (data: any) => {
+          if (data.op !== SendOP.DISPATCH_MESSAGE) return;
+          expect(data.d.cid).toBeDefined();
+          expect(data.d.data).toBeDefined();
+          res();
         });
       });
-    });
-
-    it('다른 사용자가 전송한 메세지를 수신합니다.', async () => {
-      const testMessage: Message<RecvPayload.Text> = {
-        op: RecvOP.SEND_MESSAGE,
-        d: {
-          cid: senderChannels[0],
-          data: 'hello',
-        },
-      };
-      return new Promise<void>((res, rej) => {
-        sender.send(testMessage);
-        receiver.on('message', (data) => {
-          if (data.op === SendOP.DISPATCH_MESSAGE) {
-            expect(data.d.cid).toBe(testMessage.d.cid);
-            res();
-          } else rej();
-        });
-      });
-    });
-
-    afterAll(() => {
-      sender.disconnect();
-      receiver.disconnect();
     });
   });
 });
--- test/common/Client.ts
+++ test/common/Client.ts
@@ -1,34 +1,42 @@
 import { Socket } from 'socket.io-client';
 import { io } from 'socket.io-client';
 
-export class TestClient {
-  public socket: Socket;
-  public channels: any[];
+export const TestClient = (
+  host: string,
+  options?: {
+    autoConnect?: boolean;
+    auth?: string;
+  },
+) => {
+  return io('ws://' + host, {
+    autoConnect: options?.autoConnect === undefined ? true : options?.autoConnect,
+    forceNew: true,
+    transports: ['websocket'],
+    extraHeaders: {
+      authorization: `Bearer ${options?.auth}`,
+    },
+  });
+};
 
-  constructor(NODE_HOST: string, NODE_PORT: number | string, AUTH_TOKEN: string) {
-    this.socket = io('http://' + NODE_HOST + ':' + NODE_PORT + '/v1/chat', {
-      autoConnect: false,
-      forceNew: true,
-      extraHeaders: {
-        authorization: AUTH_TOKEN,
-      },
-    });
-  }
+export const getToken = async (host: string, user: { email: string; password: string }): Promise<string> => {
+  return fetch('http://' + host + '/v1/auth/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(user),
+  }).then(async (res) =>
+    res.json().then((body) => {
+      return body.accessToken;
+    }),
+  );
+};
 
-  public init(done: jest.DoneCallback) {
-    this.socket.once('connected', (data) => {
-      this.channels = data.channels || [];
-      // log(this.channels);
-      done();
-    });
-    this.socket.connect();
-  }
-
-  public on(ev: string, listener: (...args: any[]) => void) {
-    this.socket.on(ev, listener);
-  }
-
-  public emit(ev: string, ...args: any[]) {
-    this.socket.emit(ev, ...args);
-  }
-}
+export const getChannels = async (host: string, auth: string): Promise<string[]> => {
+  return fetch('http://' + host + '/v2/channel', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + auth },
+  }).then(async (res) =>
+    res.json().then((body) => {
+      return body.channels?.map((c: any) => c.id);
+    }),
+  );
+};
