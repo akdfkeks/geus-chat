784e34f04fd9f93a51cee7ff19344804d6f0481e
akdfkeks <akdfkeks@gmail.com>
2024-03-11 15:22:00 +0900

    refact:

--- src/repository/message.repository.ts
+++ src/repository/message.repository.ts
@@ -5,28 +5,34 @@ import { MESSAGE_HISTORY } from 'src/common/constant/database';
 import { DEFULT_FIND_MESSAGE_LIMIT } from 'src/common/constant/message';
 import { SnowFlake } from 'src/common/util/snowflake';
 import { IGetChannelMessageQuery } from 'src/structure/dto/Channel';
-import { IFindMessageResult, RecvPayload, SendPayload } from 'src/structure/dto/Message';
-import { MessageSchema } from 'src/structure/model/message';
+import { Message } from 'src/structure/message';
 
 @Injectable()
 export class MessageRepository {
   constructor(@InjectConnection() private readonly mongo: Db) {}
 
+  public async saveMessage(m: Message.RecvDto & { uid: string }) {
+    const msg = Message.toModel(m);
+    return this.mongo
+      .collection<Message.Model>(MESSAGE_HISTORY)
+      .insertOne(msg)
       .then((v) => Message.toSendDto(msg));
   }
 
   public async getMessagesByQuery(query: IGetChannelMessageQuery & { channelId: string }) {
     return this.mongo
-      .collection<MessageSchema>(MESSAGE_HISTORY)
+      .collection<Message.Model>(MESSAGE_HISTORY)
       .find({
         _id: {
-          // $gte: {{ 사용자가 채팅방에 입장한 시간 }}
           $lte: query.before ? BigInt(query.before) : SnowFlake.genFake(),
         },
+        // time: {
+        // 	$gte: // 채팅방에 입장한 시간
+        // },
         channel_id: query.channelId,
       })
       .sort('_id', -1)
-      .limit(query.before ? Math.min(+query.before, DEFULT_FIND_MESSAGE_LIMIT) : DEFULT_FIND_MESSAGE_LIMIT)
+      .limit(query.before ? Math.min(Math.abs(+query.before), DEFULT_FIND_MESSAGE_LIMIT) : DEFULT_FIND_MESSAGE_LIMIT)
       .toArray()
       .then((msgs) => msgs.map(Message.toSendDto));
   }
