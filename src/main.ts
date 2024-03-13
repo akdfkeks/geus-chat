eee2c244f72feade423530969f0ccff6c1dcd23c
akdfkeks <akdfkeks@gmail.com>
2024-03-13 16:36:25 +0900

    feat: add replacer to JSON.stringify

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './module/app.module';
 import { RedisIoAdapter } from 'src/common/adapter/redis.adapter';
 import { ConfigService } from '@nestjs/config';
+import { BigIntSerializer } from 'src/common/interceptor/bigint-serializer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters();
+  app.useGlobalInterceptors(new BigIntSerializer());
   app.useWebSocketAdapter(new RedisIoAdapter(app));
 
   const configService = app.get(ConfigService);
