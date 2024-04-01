1f0fa05869e76656e18bf9fc4c5754ee63ec889b
akdfkeks <akdfkeks@gmail.com>
2024-04-02 03:06:47 +0900

    refact: 사전정의 예외객체 사용

--- src/common/pipe/parse-bigint.pipe.ts
+++ src/common/pipe/parse-bigint.pipe.ts
@@ -1,10 +1,11 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
 import { isNil } from 'src/common/util/utils';
+import * as error from 'src/common/error/http-error';
 
 export class ParseBigIntPipe implements PipeTransform {
   public transform(value: any, meta: ArgumentMetadata) {
     if (isNil(value) || !this.isParsable(value)) {
-      throw new BadRequestException();
+      throw new BadRequestException(error.BAD_REQUEST);
     }
     return BigInt(value);
   }
